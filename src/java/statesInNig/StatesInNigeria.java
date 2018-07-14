be581854de1cee588f67034490392da5730bf56a
angelis <angelis@parrot>
2018-07-14 20:28:02 +0100

    The Commit on my webpage

--- src/java/statesInNig/StatesInNigeria.java
+++ src/java/statesInNig/StatesInNigeria.java
@@ -6,39 +6,57 @@
 package statesInNig;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.PostConstruct;
+//import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+//import javax.inject.Named;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author angelis
  */
-@RequestScoped
+
 @ManagedBean(name="statesInNigeria")
+@SessionScoped
+
+//@Named(value="statesInNigeria")
+//@SessionScoped
 
 public class StatesInNigeria implements Serializable{
     
-    public List <String> states= Arrays.asList("Abia", "Adamawa", "Akwa-Ibom", "Anambra", "Bouchi");
+    private OurStates ourStates;
+    private List <String> states;
 
     
-    public List<String> getStates() {
+    public StatesInNigeria(){
+        }
+    
+    @PostConstruct
+    public void init(){
+        ourStates = new OurStates();
+        states = Arrays.asList("Abia", "Anambra", "Zamfara", "Delta", "Kebbi");
+    
+    }
+
+    public OurStates getOurStates() {
+        return ourStates;
+    }
+
+    public void setOurStates(OurStates ourStates) {
+        this.ourStates = ourStates;
+    }
+
+    public List<String> getStates(){
         return states;
     }
 
-    public void setStates(List<String> states) {
-        this.states = states;
-    }
     
-    public StatesInNigeria(){
-    }
-    
-    public void fetchStates(){
-         for(Object ob: states){
-            ob.toString();
-        }
-    }
+   
     
 }
 
